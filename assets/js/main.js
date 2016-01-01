0396bb0cc4dda1785269c5d1ed1cd023f0bec4af
Benoît Bryon <benoit@marmelune.net>
2016-01-01 21:26:36 +0100

    Improved header animation of single-page layout. Work in progress.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,15 @@
 (function($) {
     $(document).ready(function() {
-        $('body').scrollspy({
-            offset: 0,
-            method: 'position',
-            target: 'header'
-        });
-/*        $('h1').sticky();
-        $('hr.bar').sticky();
-        $('hr.bar').on('sticky-start', header_vanish);
-        $('hr.bar').on('sticky-end', header_highlight);*/
+      $('body').scrollspy({
+        offset: 0,
+        method: 'offset',
+        target: 'header'
+      });
+/*      $('h1').sticky();
+      $('hr.bar').sticky();
+      $('hr.bar').on('sticky-start', header_vanish);
+      $('hr.bar').on('sticky-end', header_highlight);*/
+      init();
     });
     $(window).on('activate.bs.scrollspy', function (e, args) {
 
@@ -19,6 +20,39 @@
     }
     function header_vanish() {
         $('h1').addClass('discreet');
+    }
+
+
+
+    var minified = false;
+    var header = $('header');
+    var didScroll = false;
+    var changeHeaderOn = 200 - 54;
+
+    function init() {
+      scrollPage();
+        $(window).scroll(function( event ) {
+            console.log('scrolling');
+            if( !didScroll ) {
+                didScroll = true;
+		scrollPage();
+            };
+        });
+    }
+
+    function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) {
+            header.addClass('minified');
+        }
+        else {
+            header.removeClass('minified');
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return $(window).scrollTop();
     }
 })(jQuery);
 
